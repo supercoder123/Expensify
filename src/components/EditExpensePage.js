141ee6197a461cb7dd45528401c6e8772fd40c48
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-12 20:46:00 +0530

    vid 081: removed edit path from header

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-const EditExpensePage = () => <div>This is from my EditPage component</div>;
+const EditExpensePage = props => {
+  console.log(props);
+  return (
+    <div>This is from my EditPage component id:{props.match.params.id}</div>
+  );
+};
 
 export default EditExpensePage;
