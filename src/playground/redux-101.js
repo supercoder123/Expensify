f4a5dcd5636bf79aed2f00ee16040064a82d366b
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-13 23:34:23 +0530

    vid 087 dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,16 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case "RESET":
       return {
@@ -19,10 +23,13 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({
-  type: "INCREMENT"
+  type: "INCREMENT",
+  incrementBy: 5
 });
 
 store.dispatch({
@@ -34,7 +41,10 @@ store.dispatch({
 });
 
 store.dispatch({
-  type: "DECREMENT"
+  type: "DECREMENT",
+  decrementBy: 10
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT"
+});
