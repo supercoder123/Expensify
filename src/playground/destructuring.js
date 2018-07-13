8d3e6a6a933b733433b03e118b3e5b9c4a22cbab
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-13 23:57:32 +0530

    vid 089: array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -14,13 +14,23 @@
 
 // console.log(`It's ${temperature} in ${city}`);
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Goo suile",
-  publisher: {
-    name: "Pingu"
-  }
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Goo suile",
+//   publisher: {
+//     name: "Pingu"
+//   }
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = "Self-Published" } = book.publisher;
+// console.log(publisherName);
+
+// const address = ["new prathamesh", "hello", "how are you", "34324"];
+
+// const [, , greeting = "New York", number] = address;
+
+// console.log(`You are in ${greeting} `);
+
+const item = ["Coffee (hot)", "$2", "$2.5", "$2.75"];
+const [coffee, , med] = item;
+console.log(`A ${coffee} costs ${med}`);
