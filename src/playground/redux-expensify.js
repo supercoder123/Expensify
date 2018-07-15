05a92fae2f5b41fa464371a960e7d418c7f0391c
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-15 19:11:57 +0530

    vid 092: combine reducers finished

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,11 +1,43 @@
 import { createStore, combineReducers } from "redux";
 
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const filtersReducerDefaultState = {
+  text: " ",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [
     {
       id: "adsfads",
       description: "Rent",
-      note: "this is a final payment",
+      note: "this is a fi nal payment",
       amount: 54500,
       createdAt: 0
     }
