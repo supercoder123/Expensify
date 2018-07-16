e6d4711f2f07f642ce1923518b04aee352d8a702
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-16 21:20:11 +0530

    vid 095: completed all the reducers and actions

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -58,6 +58,24 @@ const setTextFilter = (text = " ") => ({
   text
 });
 
+const sortByAmount = () => ({
+  type: "SORT_BY_AMOUNT"
+});
+
+const sortByDate = () => ({
+  type: "SORT_BY_DATE"
+});
+
+const setStartDate = startDate => ({
+  type: "SET_START_DATE",
+  startDate
+});
+
+const setEndDate = endDate => ({
+  type: "SET_END_DATE",
+  endDate
+});
+
 const filtersReducerDefaultState = {
   text: " ",
   sortBy: "date",
@@ -72,6 +90,27 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
         ...state,
         text: action.text
       };
+    case "SORT_BY_AMOUNT":
+      return {
+        ...state,
+        sortBy: "amount"
+      };
+    case "SORT_BY_DATE":
+      return {
+        ...state,
+        sortBy: "date"
+      };
+    case "SET_START_DATE":
+      return {
+        ...state,
+        startDate: action.startDate
+      };
+    case "SET_END_DATE":
+      return {
+        ...state,
+        endDate: action.endDate
+      };
+
     default:
       return state;
   }
@@ -88,19 +127,26 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-const expenseOne = store.dispatch(
-  addExpense({ description: "Rent", amount: 100 })
-);
-const expenseTwo = store.dispatch(
-  addExpense({ description: "Coffee", amount: 300 })
-);
+// const expenseOne = store.dispatch(
+//   addExpense({ description: "Rent", amount: 100 })
+// );
+// const expenseTwo = store.dispatch(
+//   addExpense({ description: "Coffee", amount: 300 })
+// );
 
-store.dispatch(removeExpense({ id: expenseOne.expenses.id }));
+// store.dispatch(removeExpense({ id: expenseOne.expenses.id }));
 
-store.dispatch(editExpense(expenseTwo.expenses.id, { amount: 500 }));
+// store.dispatch(editExpense(expenseTwo.expenses.id, { amount: 500 }));
 
-store.dispatch(setTextFilter("Shit"));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter("Shit"));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(3244));
 
 const demoState = {
   expenses: [
