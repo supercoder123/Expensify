b120f713abca8c70076c2955edefa5d5cfc5b13f
Ashley Fernandes <ashleefernandes@gmail.com>
2018-07-18 23:21:31 +0530

    vid 099: separated reducers in different files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import { addExpense, removeExpense, editExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import visibleExpenses from "./selectors/expenses";
+
+const store = configureStore();
+
+store.dispatch(
+  addExpense({
+    description: "Water bill",
+    note: "this is a water bill",
+    amount: 800
+  })
+);
+
+store.dispatch(
+  addExpense({
+    description: "Gas bill",
+    note: "this is a gas bill",
+    amount: 300
+  })
+);
+
+store.dispatch(setTextFilter("water"));
+
+const storeReturned = store.getState();
+console.log(visibleExpenses(storeReturned.expenses, storeReturned.filters));
 
 ReactDOM.render(AppRouter, document.getElementById("app"));
